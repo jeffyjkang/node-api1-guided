fdfb283ee52380555d3ac34a7fe4bae68a42dc25
jeff kang <jeffyjkang@gmail.com>
2022-08-25 20:34:07 -0500

    index,server,get hello

--- api/server.js
+++ api/server.js
@@ -1,12 +1,17 @@
 // IMPORTS AT THE TOP
+const express = require('express')
 
 // INSTANCE OF EXPRESS APP
+const server = express()
 
 // GLOBAL MIDDLEWARE
 
 // ENDPOINTS
 
 // [GET]    /             (Hello World endpoint)
+server.get("/", (req, res) => {
+  res.json({message: "hello"})
+})
 // [GET]    /api/dogs     (R of CRUD, fetch all dogs)
 // [GET]    /api/dogs/:id (R of CRUD, fetch dog by :id)
 // [POST]   /api/dogs     (C of CRUD, create new dog from JSON payload)
@@ -14,3 +19,4 @@
 // [DELETE] /api/dogs/:id (D of CRUD, remove dog with :id)
 
 // EXPOSING THE SERVER TO OTHER MODULES
+module.exports = server
--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 // import the server and start it!
-console.log("hello world")
+// const express = require("express");
+// const server = express();
+
+const server = require('./api/server')
+
+
+
+server.listen(3000, () => {
+  console.log('server listening on 3000')
+});
+
+// httpie
+// http get :3000 --verbose
